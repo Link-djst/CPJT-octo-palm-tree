a1f9c939fc58ef656fc1e3d554120b6254f170fa
Diego Sosa <diego.stt@live.com>
2016-05-20 07:55:33 -0600

    Test git

--- test.py
+++ test.py
@@ -8,39 +8,39 @@ import sys
 from PyQt4 import QtGui, QtCore
 
 
-class Example(QtGui.QMainWindow):
-    
+class GUI(QtGui.QMainWindow):
+
     def __init__(self):
         super(Example, self).__init__()
-        
+
         self.initUI()
-        
-    def initUI(self):      
+
+    def initUI(self):
 
         btn1 = QtGui.QPushButton("Button 1", self)
         btn1.move(30, 50)
 
         btn2 = QtGui.QPushButton("Button 2", self)
         btn2.move(150, 50)
-      
-        btn1.clicked.connect(self.buttonClicked)            
+
+        btn1.clicked.connect(self.buttonClicked)
         btn2.clicked.connect(self.buttonClicked)
-        
+
         self.statusBar()
-        
+
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Event sender')
         self.show()
-        
+
     def buttonClicked(self):
-      
+
         sender = self.sender()
         self.statusBar().showMessage(sender.text() + ' was pressed')
-        
+
 def main():
-    
+
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = GUI()
     sys.exit(app.exec_())
 
 
